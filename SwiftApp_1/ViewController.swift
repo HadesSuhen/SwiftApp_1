364eaef6ef3a5942e80bafea0f6a2bc4f0f54ac6
HadesSuhen <jjx_wxt@163.com>
2014-09-29 11:28:18 +0800

    1.1
    swift1.1

--- SwiftApp_1/ViewController.swift
+++ SwiftApp_1/ViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
                             
+    @IBOutlet var label : UILabel
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.label.text = "Hello Hades!"
     }
 
     override func didReceiveMemoryWarning() {
